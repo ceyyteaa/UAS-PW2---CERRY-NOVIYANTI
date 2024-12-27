ad53ac638da42cf0c513ae70246c0fe212829c8e
ceyyteaa <cerrynoviyanti05@gmail.com>
2024-12-27 23:04:04 +0700

    UAS WEB 2 CERRY

---
+++ app/Config/Routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('images/(:segment)', 'Home::images/$1');
+$routes->get('uploads/images/(:segment)', 'Home::images/$1');
+
+// Admin
+$routes->group('admin', ['filter' => 'group:admin'], function ($routes) {
+    $routes->get('dataproduk', 'AdminController::dataproduk');
+    $routes->post('dataproduk', 'AdminController::create_produk');
+    $routes->post('create_produk', 'AdminController::create_produk');
+    $routes->get('dataproduk/(:segment)/edit', 'AdminController::edit_produk/$1');
+    $routes->post('dataproduk/(:segment)/update', 'AdminController::update_produk/$1');
+    $routes->get('dataproduk/(:segment)/delete', 'AdminController::delete_produk/$1');
+});
+
+service('auth')->routes($routes);
